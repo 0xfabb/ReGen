57e674b2eeb7363a8aa98e32f37ca91d95e59d72
0xfabb <vanshdev2305@gmail.com>
2025-02-28 12:32:13 +0530

    Initial testing

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 
 app.get('/', (req,res)=>{
     res.send('Hello World');
 })
+app.get('/users', (req,res)=>{
+    res.json({
+        users:[
+            {name: 'John', age: 25},
+            {name: 'Jane', age: 24},
+            {name: 'Doe', age: 26}
+        ]})
+    })
 
 app.listen(port, ()=> console.log(`Server is running on port ${port}`));
